e262120f2c0eec48e2e64bd3732ced60788365b3
advokata <badanarkA>
2018-01-05 21:34:52 +0200

    Some fixes in AdapterInterface

--- src/Adapters/AdapterInterface.php
+++ src/Adapters/AdapterInterface.php
@@ -4,71 +4,111 @@ namespace EmoG\KendoGrid\Adapters;
 
 use EmoG\KendoGrid\ParamsParser;
 
+/**
+ * @property ParamsParser $parser
+ */
 abstract class AdapterInterface
 {
 
-    protected $parser = null;
-    protected $columns = [];
+    protected $parser        = null;
+    protected $columns       = [];
     protected $customColumns = [];
 
     protected $length = 30;
 
+    /**
+     * AdapterInterface constructor.
+     * @param $length
+     */
     public function __construct($length)
     {
         $this->length = $length;
     }
 
+    /**
+     * @return mixed
+     */
     abstract public function getResponse();
 
+    /**
+     * @param ParamsParser $parser
+     */
     public function setParser(ParamsParser $parser)
     {
         $this->parser = $parser;
     }
 
+    /**
+     * @param array $columns
+     */
     public function setColumns(array $columns)
     {
         $this->columns = $columns;
     }
 
+    /**
+     * @return array
+     */
     public function getColumns()
     {
         return $this->columns;
     }
 
+    /**
+     * @return array
+     */
     public function getCustomColumns()
     {
         return $this->customColumns;
     }
 
+    /**
+     * @param $customColumns
+     */
     public function setCustomColumns($customColumns)
     {
         $this->customColumns = $customColumns;
     }
 
+    /**
+     * @param $column
+     * @return bool
+     */
     public function columnExists($column)
     {
         return in_array($column, $this->columns);
     }
 
+    /**
+     * @param $column
+     * @return bool
+     */
     public function columnExistsInCustomColumns($column)
     {
         return in_array($column, array_keys($this->customColumns));
     }
 
+    /**
+     * @return ParamsParser
+     */
     public function getParser()
     {
         return $this->parser;
     }
 
+    /**
+     * @param $options
+     * @return array
+     */
     public function formResponse($options)
     {
         $defaults = [
             'total' => 0,
             'data' => []
         ];
-        $options += $defaults;
+        $options  += $defaults;
 
-        $response = [];
+        $response          = [];
         $response['total'] = $options['total'];
 
         if (count($options['data'])) {
@@ -86,16 +126,29 @@ abstract class AdapterInterface
         return $response;
     }
 
+    /**
+     * @param $string
+     * @return string
+     */
     public function sanitaze($string)
     {
         return mb_substr($string, 0, $this->length);
     }
 
+    /**
+     * @param int $length
+     * @return bool|string
+     */
     public function getRandomString($length = 7)
     {
         return substr(md5(rand()), 0, $length);
     }
 
+    /**
+     * @param $case
+     * @param $closure
+     * @throws \Exception
+     */
     public function bind($case, $closure)
     {
         switch ($case) {
@@ -103,7 +156,9 @@ abstract class AdapterInterface
                 $columnSearch = $this->parser->getColumnsSearch();
                 if (!$columnSearch) return;
                 foreach ($columnSearch as $key => $column) {
-                    if (!$this->columnExists($column['field'])) continue;
+                    if (!$this->columnExists($column['field']) && !$this->columnExistsInCustomColumns($column['field'])) {
+                        continue;
+                    }
                     $closure($column);
                 }
                 break;
@@ -116,7 +171,9 @@ abstract class AdapterInterface
                 foreach ($order as $orderBy) {
                     if (!isset($orderBy['dir']) || !isset($orderBy['field'])) continue;
                     $orderDir = mb_strtoupper($orderBy['dir']);
-                    if (!$this->columnExists($orderBy['field']) && !$this->columnExistsInCustomColumns($orderBy['field'])) continue;
+                    if (!$this->columnExists($orderBy['field']) && !$this->columnExistsInCustomColumns($orderBy['field'])) {
+                        continue;
+                    }
                     $orderArray[] = "{$orderBy['field']} {$orderDir}";
                 }
                 if (empty($orderArray)) return;
@@ -128,25 +185,29 @@ abstract class AdapterInterface
 
     }
 
+    /**
+     * @param $column
+     * @return mixed
+     */
     public function getFilterDbOperators($column)
     {
         $column['searchValue'] = $column['value'];
         switch ($column['operator']) {
             case "contains":
                 $column['searchValue'] = "%{$column['value']}%";
-                $column['dbOperator'] = "LIKE";
+                $column['dbOperator']  = "LIKE";
                 break;
             case "doesnotcontain":
                 $column['searchValue'] = "%{$column['value']}%";
-                $column['dbOperator'] = "NOT LIKE";
+                $column['dbOperator']  = "NOT LIKE";
                 break;
             case "startswith":
                 $column['searchValue'] = "{$column['value']}%";
-                $column['dbOperator'] = "LIKE";
+                $column['dbOperator']  = "LIKE";
                 break;
             case "endswith":
                 $column['searchValue'] = "%{$column['value']}";
-                $column['dbOperator'] = "LIKE";
+                $column['dbOperator']  = "LIKE";
                 break;
             case "eq":
                 $column['dbOperator'] = "=";
@@ -170,10 +231,16 @@ abstract class AdapterInterface
         return $column;
     }
 
+    /**
+     * @param $itemValue
+     * @param $searchValue
+     * @param $operator
+     * @return bool
+     */
     public function checkCondition($itemValue, $searchValue, $operator)
     {
         if (!is_numeric($itemValue) && !is_numeric($searchValue)) {
-            $itemValue = strtolower($itemValue);
+            $itemValue   = strtolower($itemValue);
             $searchValue = strtolower($searchValue);
         };
         switch ($operator) {
